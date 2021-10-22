ce2cb61dd0f8b94051d3b4e7407d54df6af4965e
Nandita24 <69765570+Nandita24@users.noreply.github.com>
2021-10-22 14:40:23 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,33 @@
+//Program 1
+
+#include<stdio.h>
+#define MAX 100
+
+int main()
+{
+	int a[MAX],i,j,k;
+	int n; int b[MAX];
+	printf("Enter no of ballons\n");
+	scanf("%d",&n);
+	printf("Enter the ballons present\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		b[i]=0;
+	}
+	printf("The ballon picked without repetation are\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				b[i]=1;
+			}
+		}
+		if(b[i]==0)
+		printf("%d\n",a[i]);
+	}
+}
+	
+		
